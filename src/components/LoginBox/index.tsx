d9fdc17913c0611cbfda58b7db33595db000ed1a
Anderson Begossi <andersonbegossi@gmail.com>
2021-11-02 14:49:34 -0300

    feat: integrate login box with github

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -3,10 +3,13 @@ import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientId}`;
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size={24} />
         Entrar com GitHub
       </a>
