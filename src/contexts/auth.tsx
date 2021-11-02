07730f13184f6fd1be418b1546d063e26cba36c7
Anderson Begossi <andersonbegossi@gmail.com>
2021-11-02 15:19:08 -0300

    chore: create auth context

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+type AuthProvider = {
+  children: ReactNode;
+};
+
+type AuthResponse = {
+  token: string;
+  user: User;
+};
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientId}`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user: userData } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+
+    setUser(userData);
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token');
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then(response => {
+        setUser(response.data);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
